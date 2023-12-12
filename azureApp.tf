59acb46fcf29d599e614622a9387ea1b85b0307d
viswanath11 <40738371+viswanath11@users.noreply.github.com>
2023-12-12 22:05:44 +0530

    Update azureApp.tf

--- azureApp.tf
+++ azureApp.tf
@@ -1,69 +1,40 @@
-terraform {
-  required_providers {
-    azurerm = {
-      version = ">=2.76.0"
-    }
-    random = {
-      version = "3.1.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
-  name     = "exampleRG1"
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-  delegation {
-    name = "Microsoft.Web.hostingEnvironments"
-    service_delegation {
-      name    = "Microsoft.Web/hostingEnvironments"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_app_service_environment_v3" "example" {
-  name                = "example-asev3"
+resource "azurerm_public_ip" "example" {
+  name                = "testpip"
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  subnet_id           = azurerm_subnet.example.id
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
-  internal_load_balancing_mode = "Web, Publishing"
+resource "azurerm_firewall" "example" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
 
-  cluster_setting {
-    name  = "DisableTls1.0"
-    value = "1"
-  }
-
-  cluster_setting {
-    name  = "InternalEncryption"
-    value = "true"
-  }
-
-  cluster_setting {
-    name  = "FrontEndSSLCipherSuiteOrder"
-    value = "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-  }
-
-  tags = {
-    env         = "development"
-    terraformed = "true"
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.example.id
+    public_ip_address_id = azurerm_public_ip.example.id
   }
 }
